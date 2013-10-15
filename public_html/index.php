4dc6389490787f5b28442b8c589898a26fde27a2
Chimo <chimo@chromic.org>
2013-10-15 12:22:46 -0400

    Use Silex, define routes

--- public_html/index.php
+++ public_html/index.php
@@ -1,15 +1,130 @@
 <?php
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
 require_once('../private/config.php');
 require_once('../private/lib/firefox-sync-client-php/sync.php');
 require_once('../private/ffobjects.php');
+require_once('../private/lib/silex/vendor/autoload.php');
 
+use Symfony\Component\HttpFoundation\Request;
+
+$app = new Silex\Application();
 $sync = new Firefox_Sync($config['username'], $config['password'], $config['sync_key'], $config['url_base']);
+// $records = $sync->collection_full('bookmarks', $limit, $offset); // TODO: Error handling
 $records = $sync->collection_full('bookmarks'); // TODO: Error handling
-
-//    $data = $sync->delete_item('bookmarks', 'vBUhSNLTvcjh');
-// print_r($data);
-
+// $bookmarks = new Bookmarks($records, $tag);
 $bookmarks = new Bookmarks($records);
-$bookmarks->html();
+
+// Twig templates
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__ . '/../private/templates'
+));
+
+// Sessions
+$app->register(new Silex\Provider\SessionServiceProvider());
+
+// List bookmarks
+$app->get('/', function () use ($app, $bookmarks) {
+    return $app['twig']->render('bookmarks.tpl', array(
+        'bookmarks'  => $bookmarks,
+        'username' => $app['session']->get('username')
+    ));
+
+});
+
+// Handle login attempt
+$app->post('/login', function (Request $request) use ($app, $config) {
+    if ($app['session']->get('username')) {
+        return $app->redirect('/');
+    }
+
+    $username = $request->get('username');
+    $password = $request->get('password');
+
+    if ($username === $config['username'] && $password === $config['password']) {
+        $app['session']->set('username', $username);
+    } else {
+        $app['session'] ->getFlashBag()->add('message', 'Incorrect username or password.');
+        return $app['twig']->render('login.tpl');
+    }
+
+    return $app->redirect('/');
+});
+
+// Login
+$app->get('/login', function () use ($app) {
+    if ($app['session']->get('username')) {
+        return $app->redirect('/');
+    }
+
+    return $app['twig']->render('login.tpl');
+});
+
+// Logout
+$app->get('/logout', function () use ($app) {
+    $app['session']->clear();
+    $app['session']->getFlashBag()->add('message', 'You are now logged out.');
+    
+    return $app->redirect('/');
+});
+
+// Show the 'add a bookmark' form
+$app->get('add', function () use ($app) {
+    if (!$app['session']->get('username')) {
+        return $app->redirect('/login');
+    }
+
+    return $app['twig']->render('add.tpl');
+});
+
+// Add bookmark
+$app->post('add', function (Request $request) use ($app, $sync) {
+    if (!$app['session']->get('username')) {
+        return $app->redirect('/login');
+    }
+
+    $foo = $app['request']->request->all();
+
+    $r = $sync->add_bookmark($foo);
+
+    if ($r) {
+        $app['session'] ->getFlashBag()->add('message', 'Bookmark successfully added');
+        return $app->redirect('/');
+    } else {
+        $app['session'] ->getFlashBag()->add('message', 'Something went wrong. Try again.'); // TODO: Better err msg.
+        return $app['twig']->render('add.tpl'); // TODO: Ensure fields are populated with previous data
+    }
+});
+
+// Delete bookmark
+$app->get('/delete/{id}', function (Silex\Application $app, $id) use ($sync) {
+    if (!$app['session']->get('username')) {
+        return $app->redirect('/login');
+    }
+
+    $r = $sync->delete_bookmark($id);
+
+    if ($r) {
+        $app['session'] ->getFlashBag()->add('message', 'Bookmark successfully deleted');
+    } else {
+        $app['session'] ->getFlashBag()->add('message', 'Something went wrong. Try again.'); // TODO: Better err msg
+    }
+
+    return $app->redirect('/');
+});
+
+// TODO: Edit bookmark
+$app->get('/edit/{id}', function ($id) use ($app) {
+
+});
+
+// TODO: Filter bookmarks by tag(s)
+$app->get('/{tag}', function ($tag) use ($app) {
+
+});
+
+$app->run();
 
 ?>
